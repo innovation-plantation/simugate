3f651c4204445dd1d5a8584796a06a6885b167b5
innovation-plantation <t.s@ieee.org>
2017-03-27 16:50:39 -1000

    Changed simugate.py to present a menu rather than popping up multiple windows.

--- simugate.py
+++ simugate.py
@@ -1,10 +1,26 @@
-import gui
 import exercises.ttl_exercise
 import exercises.ff_exercise
-
-exercises.ttl_exercise.exercise()
-gui.do_gui()
-exercises.ff_exercise.exercise()
+import circuit
+import gui
 import tkinter
+
+def launch_exercise(exercise):
+    circuit.Figure.default_canvas = None
+    main_window.destroy()
+    exercise()
+
+def gui_launch(exercise=None):
+    gui.do_gui()
+    main_window.destroy()
+    if exercise: exercise()
+
+main_window = tkinter.Tk()
+tkinter.Button(main_window,text="Flip Flop Exercise",command = lambda: launch_exercise(exercises.ff_exercise.exercise)).pack(fill='x')
+tkinter.Button(main_window,text="Transistors Exercise",command = lambda: launch_exercise(exercises.ttl_exercise.exercise)).pack(fill='x')
+tkinter.Button(main_window,text="Launch Digital Schematic Editor",command = gui_launch).pack(fill='x')
+tkinter.Button(main_window,text="Flip Flop Exercise within editor",command = lambda: gui_launch(exercises.ff_exercise.exercise)).pack(fill='x')
+tkinter.Button(main_window,text="Transistors Exercise within editor",command = lambda: gui_launch(exercises.ttl_exercise.exercise)).pack(fill='x')
 tkinter.mainloop()
+
+
 
