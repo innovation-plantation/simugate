b3b5208fd5ddf82d1b8031550bc93b4ccfd13bcb
innovation-plantation <t.s@ieee.org>
2017-04-04 23:33:45 -1000

    Adjust categorization of parts

--- gui.py
+++ gui.py
@@ -53,7 +53,6 @@ def do_gui():
         ('Output Pin', device.OutputPin),
         ('Clock', device.Clock),
         ('Character Display',device.CharDisplay),
-        ('Bus', device.Bus),
     ]:
         m_io.add_command(label=item[0], command=lambda constructor=item[1]: constructor(*somewhere()))
 
@@ -64,13 +63,15 @@ def do_gui():
         ('NPN Transistor', device.NPN),
         ('PNP Transistor', device.PNP),
         ('Diode', device.Diode),
+        # bus
+        ('Bus', device.Bus),
+        # weak
+        ('Pull-up Resistor Pack', device.PullupPack),  # for use with OC bus
+        ('Pull-up Resistor', device.Pullup),
+        ('Pull-down Resistor', device.Pulldown),
         # strong
         ('Direct voltage source', device.Source),
         ('Logic ground', device.Ground),
-        # weak
-        ('Pull-up Resistor', device.Pullup),
-        ('Pullup Resistor Pack', device.PullupPack),
-        ('Pull-down Resistor', device.Pulldown),
     ]:
         m_comp.add_command(label=item[0], command=lambda constructor=item[1]: constructor(*somewhere()))
 
@@ -96,10 +97,11 @@ def do_gui():
     # Advanced
     m_adva = tkinter.Menu(tk)   # dropdown Logic Advanced
     for item in [
+        ('3-state Driver', device.Driver),
+        ('Driver (Open Collector)', device.OCDriver),
         ('Decoder', device.Decoder),
         ('Mux', device.Mux),
         ('DMux', device.DMux),
-        ('Counter', device.Counter),
         ('Adder', device.Adder),
         ('ALU', device.ALU),
     ]:
@@ -113,12 +115,11 @@ def do_gui():
         ('D Flip-Flop (Level-triggered)', device.D_flipflop),
         ('D Flip-Flop (Edge-triggered)', device.D_edge),
         # group
-        ('3-state Driver',device.Driver),
         ('Latch', device.Latch),
+        ('Counter', device.Counter),
         ('Memory', device.Mem),
         ('ROM', device.ROM),
         # group OC
-        ('Driver (Open Collector)', device.OCDriver),
         ('Latch (Open Collector)', device.OCLatch),
         ('Memory (Open Collector)', device.OCMem),
         ('ROM (Open Collector)', device.OCROM),
@@ -127,12 +128,12 @@ def do_gui():
 
     # add the dropdowns and items to the menu
     menu.add_cascade(label="File", menu=m_file)
-    menu.add_cascade(label="I/O", menu=m_io)
-    menu.add_cascade(label="Components", menu=m_comp)
-    menu.add_cascade(label="Gates", menu=m_gate)
-    menu.add_cascade(label="Advanced", menu=m_adva)
-    menu.add_cascade(label="Storage", menu=m_stor)
-    
+    menu.add_cascade(label="I/O Components", menu=m_io)
+    menu.add_cascade(label="Discrete Components, Bus and Logic levels", menu=m_comp)
+    menu.add_cascade(label="Logic Gates", menu=m_gate)
+    menu.add_cascade(label="Drivers and Combinatorial Logic", menu=m_adva)
+    menu.add_cascade(label="Storage Devices", menu=m_stor)
+
     # View commands
     menu.add_command(label='Zoom in', command=zoom_in)
     menu.add_command(label='Zoom out', command=zoom_out)
